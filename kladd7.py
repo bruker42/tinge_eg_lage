3e30718b0b6c5590e3f9db929a0451712e949a4b
bruker42 <test_bruker@gmail.com>
2015-04-27 22:27:06 +0200

    no message

--- kladd7.py
+++ kladd7.py
@@ -3,7 +3,10 @@
 
 from graphics import *
 import time
+
+
 class Filter:
+        
     """
     Skal utføre operasjoner på bildet
     """
@@ -32,23 +35,67 @@ class Filter:
         self.bildematrise = self.historie[i]
 
     def save_file(self):
-        None
+        self.bilde.save('bilde.bmp')
 
+    def undo(self):
+        if len(self.historie) > 1:
+            self.historie.pop()
+            self.bildematrise = self.historie[-1]
+            
+    def fix_pixels(self):
+        for y, rad in enumerate(self.bildematrise):
+            for x, pixel in enumerate(rad):
+                pixels = list(pixel)
+                for i,val in enumerate(pixels):
+                    if val > 255:
+                        pixels[i] = 255
+                    elif val < 0:
+                        pixels[i] = 0
+                self.bildematrise[y][x] = pixels[0], pixels[1], pixels[2]
+        
     def increase_brigthness(self):
-        None
+        for y, rad in enumerate(self.bildematrise):
+            for x, pixel in enumerate(rad):
+                r, g, b = pixel
+                r += 10
+                g += 10
+                b += 10
+                self.bildematrise[y][x] = r, g, b
 
     def decrease_brightness(self):
-        None
-
+        for y, rad in enumerate(self.bildematrise):
+            for x, pixel in enumerate(rad):
+                r, g, b = pixel
+                r -= 10
+                g -= 10
+                b -= 10
+                self.bildematrise[y][x] = r, g, b
+    
     def flip(self):
         self.bildematrise=reversed(self.bildematrise)
-        
 
+        
     def mirror(self):
-        None
+        for y, rad in enumerate(self.bildematrise):
+            self.bildematrise[y] = reversed(rad)
 
     def blur(self):
-        None
+        for y, rad in enumerate(self.bildematrise):
+            for x, pixel in enumerate(rad):
+                teller, avg_r, avg_g, avg_b = 0, 0, 0, 0             
+                for i in range(3):
+                    for j in range(3):
+                        if (y-i<0 or x-j<0) or (y-i>self.høyde or x-j>self.bredde):
+                            teller += 1
+                            avg_r += pixel[0]
+                            avg_g += pixel[1]
+                            avg_b += pixel[2]
+                        #end if
+                    #end for
+                #end for
+                avg_r, avg_g, avg_b = avg_r/teller, avg_g/teller, avg_b/teller
+                self.bildematrise[y][x] = avg_r, avg_g, avg_b
+                        
 
     def distort_colour(self):
         None
@@ -57,19 +104,17 @@ class Filter:
         None
 
     def keep_colour(self, colour='r'):
-        for y,rad in enumerate(self.bildematrise):
-            for x,pixel in enumerate(rad):
-                 r,g,b = pixel
-                 if colour == 'r':
-                     g, b = 0, 0
-                 elif colour == 'g':
+        for y, rad in enumerate(self.bildematrise):
+            for x, pixel in enumerate(rad):
+                r,g,b = pixel
+                if colour == 'r':
+                    g, b = 0, 0
+                elif colour == 'g':
                     r, b = 0, 0
-                 else:
-                     r, g = 0, 0
-                 self.bildematrise[y][x] = [r,g,b]
+                else:
+                    r, g = 0, 0
+                self.bildematrise[y][x] = [r,g,b]
                 
-
-
     def increase_contrast(self):
         None
 
@@ -91,10 +136,8 @@ class Filter:
     def sepia(self):
         None
 
-    def timing(self):
-        None
-
     def update(self):
+        self.fix_pixels()
         matrise = []
         for rad in self.bildematrise:
             radmatrise = []
@@ -107,19 +150,32 @@ class Filter:
     def hexcode(self,r,g,b):
         return "#%02x%02x%02x" % (r, g, b)
 
+
 class Kontroll:
     """
     Skal fordele funksjone til knapper i kontrollpanelet
     """
     def __init__(self):
         None
+
+
 class Knapp:
     """
     Skal lager knapper
     """
     def __init__(self):
         None
-
+        
+def bilde_til_rgb_matrise(img):
+    bredde = img.getWidth()
+    høyde = img.getHeight()
+    bildematrise = []
+    for y in range(høyde):
+            bildematrise.append([])
+            for x in range(bredde):
+                bildematrise[y].append(img.getPixel(x, y))
+    return bildematrise
+    
 def main(bildevalg='bilde.gif'):
     img = Image(Point(0, 0), bildevalg) 
     bredde = img.getWidth()
@@ -128,20 +184,14 @@ def main(bildevalg='bilde.gif'):
     vindu = GraphWin('Bilde', bredde, høyde)
     img.draw(vindu)
     
-    bildematrise = []
-    for y in range(høyde):
-            bildematrise.append([])
-            for x in range(bredde):
-                bildematrise[y].append(img.getPixel(x, y))
+    bildematrise = bilde_til_rgb_matrise(img)
     
     test = Filter(img, bildematrise, bredde, høyde, vindu)
 
-    test.flip()
-    test.update()
-    test.load_state()
+    test.mirror()
     test.update()
 
-main()
+#main()
 
     
     
